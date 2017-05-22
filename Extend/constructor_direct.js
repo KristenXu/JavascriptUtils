c3f3426d0595b9bf559eb56c7b7f94872aaed372
Chainn <578579786@qq.com>
2017-05-22 21:37:04 +0800

    Add direct for constructor extend

---
+++ Extend/constructor_direct.js
@@ -0,0 +1,16 @@
+var Car = require('./car')
+function extend(Child, Parent) {
+    Child.prototype = Parent.prototype;
+    Child.prototype.constructor = Child;
+    Child.uber = Parent.prototype;
+}
+function Audi(name,color){
+    this.name = name;
+    this.color = color;
+}
+
+extend(Audi, Car);
+var audi1 = new Audi("A6","白色");
+console.log(audi1);
+console.log(audi1.category); // 汽车
+console.log(Car.prototype.constructor); // Audi， 把父类的constructor给改了
