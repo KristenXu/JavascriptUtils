3f144e8ec1c66e677985845c7a9af3ee277ba440
Chainn <578579786@qq.com>
2017-05-22 21:34:20 +0800

    Add indirect for constructor extend

---
+++ Extend/constructor_indirect.js
@@ -0,0 +1,19 @@
+var Car = require('./car')
+function extend(Child, Parent) {
+
+    var F = function(){};
+    F.prototype = Parent.prototype;
+    Child.prototype = new F();
+    Child.prototype.constructor = Child;
+    Child.uber = Parent.prototype;
+}
+function Audi(name,color){
+    this.name = name;
+    this.color = color;
+}
+
+extend(Audi, Car);
+var audi1 = new Audi("A6","白色");
+console.log(audi1);
+console.log(audi1.category); // 汽车
+console.log(Car.prototype.constructor); // Car
