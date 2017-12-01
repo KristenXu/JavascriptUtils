4993d3a38de5dd27e8f6408a612d1c258a5a8741
Chainn <578579786@qq.com>
2017-12-01 18:09:20 +0800

    Update

--- Research/Async/generator.js
+++ Research/Async/generator.js
@@ -1,12 +1,19 @@
 let a = [1,2,3,4,5,6]
+let self = this;
 function *gen() {
     for(let value of a)
         yield console.log('value', value)
+    console.log('end')
+    clearTimeout(self.timeoutId)
 }
 
 var g = gen();
 
 
-this.timeoutId = setInterval(() => {
+function run() {
     g.next()
-}, 1000)
+    clearTimeout(self.timeoutId)
+    self.timeoutId = setTimeout(run, 200)
+}
+
+run()
