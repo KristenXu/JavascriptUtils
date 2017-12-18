81349e0b636874b9e5868212a4353c0ab5438ae7
Chainn <578579786@qq.com>
2017-12-18 10:56:55 +0800

    rename

--- Research/New/fakeNew2.js
+++ Research/New/fakeNew2.js
@@ -1,7 +1,7 @@
 /**
  * Created by chainn on 2017/12/15.
  */
-function fakeNew1 () {
+function fakeNew2 () {
     var Constructor = [].shift.call(arguments);
     var obj = {};
     obj.__proto__ = Constructor.prototype;
@@ -18,7 +18,7 @@ var Student = function (name, age) {
 Student.prototype.grade = 1;
 
 // var xiaoming = new Student('xiaoming', 8);
-var xiaoming = fakeNew1(Student, 'xiaoming', 8);
+var xiaoming = fakeNew2(Student, 'xiaoming', 8);
 
 console.log(xiaoming.name);
 console.log(xiaoming.age);
